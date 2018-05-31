0a61b86a15efec9e6b44da266b772362ad81cb20
Agustin Ducca <ADucca@devesa.com>
2018-05-31 02:05:37 -0300

    adding models news

--- js/main.js
+++
@@ -1,5 +0,0 @@
-	var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre`");
-	var f=new Date();
-	var dateShow = document.getElementById("dateField");
-	dateShow.innerHTML = f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear();
-	
